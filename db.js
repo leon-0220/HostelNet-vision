1c5b49fada1ce386dd9a441d384abd83b9840b43
leon <rsuhayra25@gmail.com>
2025-10-12 22:42:08 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,    // contoh: db-mysql-12345.railway.app
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  host: crossover.proxy.rlwy.net,
+  user: root,
+  password: uWSKTbteHaXWZipnkABQiVSUvuhZVTda,
+  database: railway,
+  port: 59855,
 });
 
 export default db;
