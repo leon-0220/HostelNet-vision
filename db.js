635e42b656d2591fa8945d104d011eab13c3c1bf
leon <rsuhayra25@gmail.com>
2025-10-12 19:46:48 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 import mysql from "mysql2/promise";
 
-// Gunakan environment variables
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,    // dari Railway
+const db = mysql.createPool({
+  host: process.env.DB_HOST,    // contoh: db-mysql-12345.railway.app
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: 3306,                   // default MySQL port
+  port: process.env.DB_PORT || 3306,
 });
 
-export default connection;
+export default db;
