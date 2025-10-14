77ce641ab29107635690bd34687c4d19141af5d8
leon <rsuhayra25@gmail.com>
2025-10-14 13:08:43 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: crossover.proxy.rlwy.net,
-  user: root,
-  password: uWSKTbteHaXWZipnkABQiVSUvuhZVTda,
-  database: railway,
+  host: "crossover.proxy.rlwy.net",
+  user: "root",
+  password: "uWSKTbteHaXWZipnkABQiVSUvuhZVTda",
+  database: "railway",
   port: 59855,
 });
 
