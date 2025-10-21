bb2dc72d6727bce314b35981a2ecb1b9c757c2a3
Tar02Ra <rsuhayra20@gmail.com>
2025-10-21 10:42:18 +0800

    update

--- db.js
+++ db.js
@@ -1,16 +1,6 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-<<<<<<< HEAD
-  host: crossover.proxy.rlwy.net,
-  user: root,
-  password: uWSKTbteHaXWZipnkABQiVSUvuhZVTda,
-  database: railway,
-  port: 59855,
-});
-
-export default db;
-=======
   host: "gondola.proxy.rlwy.net",
   user: "root",
   password: "JwOzMilejTKDdMkSNJklrBplJbYzXQNo",
@@ -19,4 +9,3 @@ export default db;
 });
 
 export default db;
->>>>>>> 1901beb2f163c484de401e79da921f161dd7742f
