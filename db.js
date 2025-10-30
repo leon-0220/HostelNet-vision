5ef69aa464de198147617b9d7e6ab57ea2f16f6a
Tar02Ra <rsuhayra20@gmail.com>
2025-10-30 18:53:56 +0800

    update

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "hostelnet-0220-hostelnet-db.g.aivencloud.com",
-  user: "avnadmin",
-  password: "AVNS_exgb7fcLJ3IbAbkYyjk",
-  database: "defaultdb",
-  port: 13379,
+  host: "centerbeam.proxy.rlwy.net",
+  user: "root",
+  password: "qxFWSgDsLImTcNYatgzJuVlcyXFAKezT",
+  database: "railway",
+  port: 37606,
 });
 
 export default db;
